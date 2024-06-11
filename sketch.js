ff65851dbb6c3c928576342a69601ac65045934a
raissagoliver0 <00001110977748sp@al.educacao.sp.gov.br>
2024-06-10 21:38:13 -0300

    adicionado arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(600, 600);
+    background("black");
+  }
+  
+  function draw() {
+    fill("purple")
+    stroke("blue")
+    
+   if(mouseIsPressed) {
+   } 
+    
+    rect(mouseX, mouseY, 20, 20);
+    
+      
+  }
